8450a95d95fa30023eeed7111c28f644cef1c747
Caio Leandro <caioleandro38@gmail.com>
2022-02-09 17:08:53 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);  
-  await app.listen(3000);
+
+  app.enableCors()
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }))
+
+  const config = new DocumentBuilder()
+  .setTitle('Gestão de comércio - APIs')
+  .setDescription('Gestão de comércio documentation API')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  try {
+    await app.listen(3000);
+
+    console.log(`Server running on port 3000!`)
+  } catch (error) {
+    console.log(`Error on running server: `, error)
+  }
 }
 bootstrap();
