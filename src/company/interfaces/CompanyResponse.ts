f4a761ccce0e4c64638d62e8fa9f77ecd1c7801f
Caio Leandro <caioleandro38@gmail.com>
2022-02-09 17:08:48 -0300

    Create CompanyResponse.ts

---
+++ src/company/interfaces/CompanyResponse.ts
@@ -0,0 +1,11 @@
+export interface CompanyResponse {
+    id_company: number,
+    name: string,
+    phone: string,
+    cnpj: string,
+    email: string,
+    address: string,
+    district: string,
+    number: string,
+    zipCode: string
+}
